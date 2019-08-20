4ec8398684b1d623facd96680f2b6601c563b29b
MarcoAFC <marcoaferc@gmail.com>
2019-08-20 16:32:12 -0300

    Uploading project

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:gif_search/home_page.dart';
+import 'gif_page.dart';
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white)
+  ));
+}
